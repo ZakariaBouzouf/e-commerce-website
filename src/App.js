83fb07574fec9db19119de8588717729cf985016
Zakaria Bouzouf <zakaria.bouzouf@gmail.com>
2024-09-23 17:45:50 +0200

    Creating the base Project and adding Bootstrap and routing to home page and login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import Root from "./pages/Root";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <div>
+      {/* <form> */}
+      {/*   <input type="text" name="email" value="email"/> */}
+      {/*   <input type="text" name="email" value="email"/> */}
+      {/* </form> */}
+      Login
+    </div>
+  )
+}
