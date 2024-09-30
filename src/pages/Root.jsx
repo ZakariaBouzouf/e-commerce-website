66e0c5e0470ac0786338579195537bb133064109
Zakaria Bouzouf <zakaria.bouzouf@gmail.com>
2024-09-30 13:38:20 +0200

    Applying the Auth context on the project

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import { AuthProvider } from "../security/AuthContext";
 
 export default function Root() {
   return (
-    <div>
+    <AuthProvider>
       <Header />
       <Outlet />
-    </div>
+    </AuthProvider>
   )
 }
