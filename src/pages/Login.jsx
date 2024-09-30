fb6604cae55eedc276e2e06f3a5a7d701fa46732
Zakaria Bouzouf <zakaria.bouzouf@gmail.com>
2024-09-30 13:41:59 +0200

    Adding basic login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,37 @@
+import { Field, Form, Formik } from "formik"
+import { SignIn } from "../components/SignIn"
+import RegisterForm from "../components/RegisterForm"
+import { useAuth } from "../security/AuthContext"
+
 export default function Login() {
+  const {isAuthenticated} = useAuth()
+
   return (
     <div>
-      {/* <form> */}
-      {/*   <input type="text" name="email" value="email"/> */}
-      {/*   <input type="text" name="email" value="email"/> */}
-      {/* </form> */}
-      Login
-    </div>
+      <h1>Login</h1>
+      <h2>Enter Your Credentials</h2>
+      {/* <button onClick={loginHandler}>Check</button> */}
+      {!isAuthenticated ? <SignIn/> : <RegisterForm/> }
+
+      {/* <div> */}
+      {/*   <Formik */}
+      {/*     initialValues={{ */}
+      {/*       email: "", */}
+      {/*       password: "", */}
+      {/*     }} */}
+      {/*     onSubmit={submit} */}
+      {/*   > */}
+      {/*     <Form> */}
+      {/*       <label htmlFor="email">Email</label> */}
+      {/*       <Field id="email" name="email" placeholder="Insert your Email" type="email" /> */}
+      {/**/}
+      {/*       <label htmlFor="password">password</label> */}
+      {/*       <Field id="password" name="password" placeholder="Insert your Password" type="password" /> */}
+      {/**/}
+      {/*       <button type="submit">Submit</button> */}
+      {/*     </Form> */}
+      {/*   </Formik> */}
+      {/* </div> */}
+    </div >
   )
 }
