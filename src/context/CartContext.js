d001293d18519efa1a5e7c5512f15e3f7f54fc0c
Zakaria Bouzouf <zakaria.bouzouf@gmail.com>
2024-10-11 17:25:44 +0200

    Adding the addToCard context and fixing the card issue with prevent the event Propagation

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -24,11 +24,17 @@ export const useCart = () => useContext(CartContext);
 export function CartProvider({ children }) {
   const [state, dispatch] = useReducer(cartReducer, initialState);
 
+  function addToCart(product) {
+    // console.log("Z", product.id);
+    dispatch({ type: "ADD_TO_CART", payload: product });
+  }
+
   return (
     <CartContext.Provider
       value={{
         numberItems: state.cartItems.length,
         cartItems: state.cartItems,
+        addToCart,
         dispatch,
       }}
     >
